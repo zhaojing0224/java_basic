e8a35c78d1faf5056fbb58eacb4454ab8298e997
zhaojing0224 <140784928+zhaojing0224@users.noreply.github.com>
2023-08-31 20:21:28 +0900

    001.設計書の読み取り 練習

---
+++ CompanyObjTry.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trymyself.newpackage;
+
+/**
+ *
+ * @author user
+ */
+public class CompanyObjTry {
+    
+    private String companyName;
+    private String position;
+    private String name;
+    private String email;
+    
+    public String getCompanyName(){
+        return companyName;
+    }
+    public void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public void setPosition(String position){
+        this.position = position;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+     public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
