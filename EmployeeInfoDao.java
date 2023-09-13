b3a69954bd2602bd47dafef52e260e0d22b456a2
zhaojing0224 <140784928+zhaojing0224@users.noreply.github.com>
2023-09-13 20:36:19 +0900

    20230913__会社事務労務会計システム__dao

--- EmployeeInfoDao.java
+++ EmployeeInfoDao.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import static java.util.Collections.list;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -90,7 +91,74 @@ public class EmployeeInfoDao {
 
     }
 
-    public List<EmployeeObj> getEmployeeInfo() {
+    /**
+     * 従業員情報編集
+     *
+     * @return
+     */
+    public void updateEmployeeInfo(EmployeeObj employeeObj) {
+
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/zhao";
+        String username = "postgres";
+        String password = "postgres";
+
+        // 获取当前系统时间
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        try {
+            // 1. 注册 PostgreSQL 驱动程序
+            Class.forName("org.postgresql.Driver");
+            // 2. 建立数据库连接
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // 3. 创建 SQL 语句
+            // String sql = "INSERT INTO EmployeeInfoDao (Name, Katakana, Gender, Birth, Phone_Number, Email, Date_of_Joining, Date_of_Resignation, Postal_Code, Address, Nationality, Contract_Type, Salary_Type, Job_Title, Office_Location, Position, Health_Insurance_Number, Labor_Insurance_Number, Period_of_Stay, Residence_Status, Deletion_Flag, Creation_Date, Update_Date) VALUES"
+            //       + "(jTextField1', 'jTextField4', 'jTextField5', 'jTextField6', 'jTextField2', 'jTextField7', 'jTextField8', 'jTextField9', 'jTextField10', 'jTextField11', 'jTextField13', 'jTextField14', 'jTextField15', 'jTextField16', 'jTextField19', 'jTextField20', 'jTextField21', 'jTextField22', 'jTextField23', 'jTextField24');";
+            StringBuffer sb = new StringBuffer();
+            
+            sb.append("UPDATE employeeInfo SET ");
+            sb.append("name = '" + employeeObj.getName() + "', ");
+            sb.append("katakana = '" + employeeObj.getKatakana() + "', ");
+            sb.append("gender = '" + employeeObj.getGender() + "', ");
+            sb.append("birth = '" + employeeObj.getBirth() + "', ");
+            sb.append("phone_number = '" + employeeObj.getPhoneNumber() + "', ");
+            sb.append("email = '" + employeeObj.getEmail() + "', ");
+            sb.append("date_of_joining = '" + employeeObj.getDateOfJoining() + "', ");
+            sb.append("date_of_resignation = '" + employeeObj.getDateOfResignation() + "', ");
+            sb.append("postal_code = '" + employeeObj.getPostalCode() + "', ");
+            sb.append("address = '" + employeeObj.getAddress() + "', ");
+            sb.append("nationality = '" + employeeObj.getNationality() + "', ");
+            sb.append("contract_type = '" + employeeObj.getContractType() + "', ");
+            sb.append("salary_type = '" + employeeObj.getSalaryType() + "', ");
+            sb.append("job_title = '" + employeeObj.getJobTitle() + "', ");
+            sb.append("health_insurance_number = '" + employeeObj.getHealthInsuranceNumber() + "', ");
+            sb.append("labor_insurance_number = '" + employeeObj.getLaborInsuranceNumber() + "', ");
+            sb.append("residence_card_number = '" + employeeObj.getResidenceCardNumber() + "', ");
+            sb.append("period_of_stay = '" + employeeObj.getPeriodOfStay() + "', ");
+            sb.append("residence_status = '" + employeeObj.getResidenceStatus() + "' ");
+            sb.append("WHERE employee_code = '" + employeeObj.getEmployeeCode() + "'");
+
+            System.out.println(sb.toString());
+
+            // 4. 创建 Statement 对象
+            Statement statement = connection.createStatement();
+
+            // 5. 执行查询并获取结果集
+            statement.executeUpdate(sb.toString());
+
+            // 7. 关闭资源
+            statement.close();
+            connection.close();
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnJdbc.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnJdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public List<EmployeeObj> getEmployeeInfoList() {
 
         System.out.println("ccccccc");
         List<EmployeeObj> list = new ArrayList<EmployeeObj>();
@@ -170,11 +238,13 @@ public class EmployeeInfoDao {
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(ConnJdbc.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return list;
     }
 
     public List<EmployeeObj> setEmployeeList() throws SQLException {
+
+        System.out.println("aaaaa");
 
         List<EmployeeObj> list = new ArrayList<EmployeeObj>();
 
@@ -189,7 +259,7 @@ public class EmployeeInfoDao {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
 
             // 3. 创建 SQL 语句
-            String sql = "SELECT * FROM EmployeeInfo WHERE employeeCode = ?";
+            String sql = "SELECT * FROM EmployeeInfo";
 
             // 4. 创建 Statement 对象
             Statement statement = connection.createStatement();
@@ -199,7 +269,7 @@ public class EmployeeInfoDao {
 
             // 6. 处理结果集
             while (resultSet.next()) {
-                String employeeCode = resultSet.getString("employeeCode");
+                String employeeCode = resultSet.getString("employee_code");
                 String name = resultSet.getString("name");
 
                 EmployeeObj obj = new EmployeeObj();
@@ -218,10 +288,133 @@ public class EmployeeInfoDao {
             Logger.getLogger(ConnJdbc.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
         return list;
+    }
+
+    public List<EmployeeObj> searchEmployeeList() {
+
+        List<EmployeeObj> list = new ArrayList<>();
+
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/zhao";
+        String username = "postgres";
+        String password = "postgres";
+
+        try {
+            // 1. 注册 PostgreSQL 驱动程序
+            Class.forName("org.postgresql.Driver");
+            // 2. 建立数据库连接
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // 3. 创建 SQL 语句
+            String sql = "SELECT * FROM EmployeeInfo WHERE employee_code = ?";
+
+            // 4. 创建 Statement 对象
+            Statement statement = connection.createStatement();
+
+            // 5. 执行查询并获取结果集
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // 6. 处理结果集
+            while (resultSet.next()) {
+                String employeeCode = resultSet.getString("employee_code");
+                String name = resultSet.getString("name");
+
+                EmployeeObj obj = new EmployeeObj();
+                obj.setEmployeeCode(employeeCode);
+                obj.setName(name);
+                list.add(obj);
+
+            }
+
+            // 7. 关闭资源
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ConnJdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public EmployeeObj getEmployeeInfo(String emCode) {
+
+        EmployeeObj obj = new EmployeeObj();
+
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/zhao";
+        String username = "postgres";
+        String password = "postgres";
+
+        try {
+            // 1. 注册 PostgreSQL 驱动程序
+            Class.forName("org.postgresql.Driver");
+            // 2. 建立数据库连接
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // 3. 创建 SQL 语句
+            String sql = "select * from EmployeeInfo where employee_code = '" + emCode + "';";
+
+            // 4. 创建 Statement 对象
+            Statement statement = connection.createStatement();
+
+            System.out.println(sql);
+            // 5. 执行查询并获取结果集
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // 6. 处理结果集
+            while (resultSet.next()) {
+                String employeeCode = resultSet.getString("employee_code");
+                String name = resultSet.getString("name");
+                String katakana = resultSet.getString("katakana");
+                String birth = resultSet.getString("birth");
+                String phoneNumber = resultSet.getString("phone_number");
+                String email = resultSet.getString("email");
+                String dateOfJoining = resultSet.getString("date_of_joining");
+                String dateOfResignation = resultSet.getString("date_of_resignation");
+                String postalCode = resultSet.getString("postal_code");
+                String address = resultSet.getString("address");
+                String nationality = resultSet.getString("nationality");
+                String contractType = resultSet.getString("contract_type");
+                String salaryType = resultSet.getString("salary_type");
+                String jobTitle = resultSet.getString("job_title");
+                String healthInsuranceNumber = resultSet.getString("health_insurance_number");
+                String laborInsuranceNumber = resultSet.getString("labor_insurance_number");
+                String residenceCardNumber = resultSet.getString("residence_card_number");
+                String periodOfStay = resultSet.getString("period_of_stay");
+                String residenceStatus = resultSet.getString("residence_status");
+
+                obj.setEmployeeCode(employeeCode);
+                obj.setName(name);
+                obj.setKatakana(katakana);
+                obj.setBirth(birth);
+                obj.setPhoneNumber(phoneNumber);
+                obj.setEmail(email);
+                obj.setDateOfJoining(dateOfJoining);
+                obj.setDateOfResignation(dateOfResignation);
+                obj.setPostalCode(postalCode);
+                obj.setAddress(address);
+                obj.setNationality(nationality);
+                obj.setContractType(contractType);
+                obj.setSalaryType(salaryType);
+                obj.setJobTitle(jobTitle);
+                obj.setHealthInsuranceNumber(healthInsuranceNumber);
+                obj.setLaborInsuranceNumber(laborInsuranceNumber);
+                obj.setResidenceCardNumber(residenceCardNumber);
+                obj.setPeriodOfStay(periodOfStay);
+                obj.setResidenceStatus(residenceStatus);
+
+            }
+
+            // 7. 关闭资源
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ConnJdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return obj;
     }
 
 }
